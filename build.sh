1b17b1fad016597a2d77e54bf0d7113457f69522
R4y <quartz010@outlook.com>
2018-02-20 15:06:39 +0800

    modified in 2018-02-20

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+echo "building... wait a while"
+gitbook build ./ docs > build.log &
 
-gitbook build ./ docs > build.log
+b=''
+for ((i=0;$i<=100;i+=2))
+do
+        printf "progress:[%-50s]%d%%\r" $b $i
+        sleep 0.1
+        b=#$b
+done
+echo
+echo "build Completed! Gitting..."
+
 git add .
-git commit -m `date +%Y-%m-%d`
+git commit -m "modified in `date +%Y-%m-%d`"
 
 
