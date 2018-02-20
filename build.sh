cf309f573224e054b5b60119eeeb0dd926277bef
R4y <quartz010@outlook.com>
2018-02-20 15:32:23 +0800

    modified in 2018-02-20

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 finish=0
+fin=1
 echo "building... wait a while"
 # gitbook build ./ docs > build.log 
 sleep 2  && finish=1 &
 
 b=''
 
-for ((i=0;"$finish" -ne 1;i+=2))
+for ((i=0;"$finish" -ne "$fin";i+=2))
 	do
         	printf "progress:[%-50s]%d%%\r" $b $i
 	        sleep 0.1
